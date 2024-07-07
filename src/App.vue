11c346cba4d8bc8e7623c7c6b1bbe446b66a61d8
utsav <utsavadhikari452@gmail.com>
2024-07-07 14:58:09 +0545

    first

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<style>
+@import "./style/app.css";
+</style>
+
+<script setup>
+import { ref } from "vue";
+
+const visible = ref(false);
+</script>
+
+<template>
+  <header>
+    <h1 class="title">Test</h1>
+    <div class="social">
+      <ul class="social-list">
+        <li class="links">F</li>
+        <li class="links">T</li>
+        <li class="links">L</li>
+      </ul>
+    </div>
+  </header>
+  <nav>
+    <ul class="nav-container">
+      <li class="nav-item">Home</li>
+      <li class="nav-item">Categories</li>
+      <li class="nav-item">Profile</li>
+      <li class="nav-item">Blog</li>
+    </ul>
+  </nav>
+</template>
