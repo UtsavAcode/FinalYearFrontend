2512c34b259693b3682d1140b06cbee1d99982d3
Utsav Adhikari <utsavadhikari452@gmail.com>
2024-09-21 14:33:32 +0545

    like and comment to add

--- Blog_App/src/components/UserActivity/UserBlogDetail.vue
+++ Blog_App/src/components/UserActivity/UserBlogDetail.vue
@@ -1,23 +1,30 @@
 <template>
-  <div v-if="blog">
-    <h1>{{ blog.title }}</h1>
-    <div class="author-date">
-      <span>By {{ blog.authorName }} on {{ formatDate(blog.createdAt) }}</span>
+  <div class="container" v-if="blog" style="width: 80%">
+    <h1 class="" style="margin: auto; width: 55rem">{{ blog.title }}</h1>
+    <div
+      class="author-date d-flex justify-content-between"
+      style="width: 59rem; margin-left: 9rem; font-size: 1em;"
+    >
+      <div>By: {{ blog.authorName }}</div>
+      <div>{{ formatDate(blog.createdAt) }}</div>
     </div>
-    <!-- Blog Image -->
-    <img
-      v-if="blog.featuredImagePath"
-      :src="getImageUrl(blog.featuredImagePath)"
-      alt="Blog Image"
-    />
-    <p>{{ blog.content }}</p>
-
     <!-- Tags -->
-    <div class="blogtags">
+    <div class="blogtags" style="width: 59rem; margin-left: 10rem">
       <span v-for="tag in blog.tags" :key="tag.id" class="badge bg-secondary">
         {{ tag.name }}
       </span>
     </div>
+    <!-- Blog Image -->
+    <div class="blogdetail-image">
+      <img
+        v-if="blog.featuredImagePath"
+        :src="getImageUrl(blog.featuredImagePath)"
+        alt="Blog Image"
+      />
+    </div>
+
+    <!-- content section -->
+    <div class="container" v-html="blog.content" style="width: 80%"></div>
   </div>
   <div v-else>Loading blog...</div>
 </template>
